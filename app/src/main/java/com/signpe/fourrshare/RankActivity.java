400f08241ac0361bdf8bafcc39c3115eaa585c9d
rladmstn <oz33177351@gamil.com>
2022-10-31 17:23:06 +0900

    랭킹 View 스크롤 및 체크박스 구현

---
+++ app/src/main/java/com/signpe/fourrshare/RankActivity.java
@@ -0,0 +1,14 @@
+package com.signpe.fourrshare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RankActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rank);
+    }
+}
