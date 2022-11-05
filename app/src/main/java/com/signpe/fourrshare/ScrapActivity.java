223875f68818aba9e9eb200d50475e181ef66966
j-nary <dlwls0921@naver.com>
2022-11-05 22:36:18 +0900

    Scrap Activity 기본

---
+++ app/src/main/java/com/signpe/fourrshare/ScrapActivity.java
@@ -0,0 +1,14 @@
+package com.signpe.fourrshare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ScrapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scrap);
+    }
+}
