9fd20c78115323ecc278d2e9c37ef94c86e08554
rladmstn <oz33177351@gmail.com>
2022-11-25 18:04:08 +0900

    유저 닉네임 변경 기능 완료

--- app/src/main/java/com/signpe/fourrshare/MyInfoActivity.java
+++ app/src/main/java/com/signpe/fourrshare/MyInfoActivity.java
@@ -6,17 +6,30 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputConnection;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 public class MyInfoActivity extends AppCompatActivity {
-
+    FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_info);
+        mAuth = FirebaseAuth.getInstance();
         Button logout = findViewById(R.id.logout_button);
+
+        EditText nameEdit = findViewById(R.id.username);
+        if (nameEdit.getText().toString() != null){
+            nameEdit.setText(mAuth.getCurrentUser().getDisplayName());
+        }
+
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -24,6 +37,23 @@ public class MyInfoActivity extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(), LoginActivity.class));
             }
         });
+    }
+
+    // 이름 변경 후 저장 버튼 누르기 ( 일단 코드는 완성 실행은 아직 X )
+    public void onClickChangeName(View v){
+        EditText nameEdit = (EditText)findViewById(R.id.username);
+        InputMethodManager imm;
+        imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(nameEdit.getWindowToken(), 0);
+        String name = nameEdit.getText().toString();
+        UserProfileChangeRequest profileChangeRequest = new UserProfileChangeRequest.Builder().setDisplayName(name).build();
+        mAuth.getCurrentUser().updateProfile(profileChangeRequest).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void unused) {
+                Toast.makeText(MyInfoActivity.this, "이름 변경 완료!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        nameEdit.setText(nameEdit.getText());
     }
 
     // 네비게이션 바
