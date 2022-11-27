33c64ecf33b75d58d0e4efed4b7f8b3834ec84d8
j-nary <dlwls0921@naver.com>
2022-11-27 21:38:05 +0900

    확대 다이얼로그 -ing

--- app/src/main/java/com/signpe/fourrshare/ExtensionDialog.java
+++ app/src/main/java/com/signpe/fourrshare/ExtensionDialog.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.CompoundButton;
+import android.widget.ImageView;
 import android.widget.Switch;
 
 public class ExtensionDialog extends AppCompatActivity {
@@ -12,8 +13,11 @@ public class ExtensionDialog extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_extension_dialog);
+
+        ImageView iv = findViewById(R.id.extensionImage);
+
         Switch switchButton = findViewById(R.id.sw);
-        return switchButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        switchButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
                 if (isChecked) {
@@ -22,7 +26,7 @@ public class ExtensionDialog extends AppCompatActivity {
                     // switchButton이 체크되지 않은 경우
                 }
             }
-        }
+        });
     }
 
 }
